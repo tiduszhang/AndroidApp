0c9343e19660ac2ae0a7f3727d6cfee426986a4d
zhangsx <zhangsx@nj-strongit.com.cn>
2018-07-17 17:27:57 +0800

    1

---
+++ app/src/main/java/com/strongit/androidapp/App/AppMain.java
@@ -0,0 +1,10 @@
+package com.strongit.androidapp.App;
+
+import me.goldze.mvvmhabit.base.BaseApplication;
+
+public class AppMain extends BaseApplication {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}

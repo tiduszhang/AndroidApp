14359dea5c096fedeecda875a89c96266de5928c
zhangsx <zhangsx@nj-strongit.com.cn>
2018-07-26 14:19:56 +0800

    DEMO - 左滑 置顶 删除

---
+++ app/src/main/java/com/strongit/androidapp/utils/adapter/TwinklingRefreshLayoutViewAdapter.java
@@ -0,0 +1,34 @@
+package com.strongit.androidapp.utils.adapter;
+
+import android.databinding.BindingAdapter;
+import com.lcodecore.tkrefreshlayout.RefreshListenerAdapter;
+import com.lcodecore.tkrefreshlayout.TwinklingRefreshLayout;
+import me.goldze.mvvmhabit.binding.command.BindingCommand;
+
+/**
+ * Created by goldze on 2017/6/16.
+ * TwinklingRefreshLayout列表刷新的绑定适配器
+ */
+public class TwinklingRefreshLayoutViewAdapter {
+
+    @BindingAdapter(value = {"onRefreshCommand", "onLoadMoreCommand"}, requireAll = false)
+    public static void onRefreshAndLoadMoreCommand(TwinklingRefreshLayout layout, final BindingCommand onRefreshCommand, final BindingCommand onLoadMoreCommand) {
+        layout.setOnRefreshListener(new RefreshListenerAdapter() {
+            @Override
+            public void onRefresh(TwinklingRefreshLayout refreshLayout) {
+                super.onRefresh(refreshLayout);
+                if (onRefreshCommand != null) {
+                    onRefreshCommand.execute();
+                }
+            }
+
+            @Override
+            public void onLoadMore(TwinklingRefreshLayout refreshLayout) {
+                super.onLoadMore(refreshLayout);
+                if (onLoadMoreCommand != null) {
+                    onLoadMoreCommand.execute();
+                }
+            }
+        });
+    }
+}
